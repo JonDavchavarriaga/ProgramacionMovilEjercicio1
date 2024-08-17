490db9dc1c69754223a622386305563fc935e344
Jonatan Chavarriaga <jonatanchavarriaga@gmail.com>
2024-08-17 13:10:23 -0500

    FinishCommit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.MainContainer}>
+      <View style={styles.containerA}> </View>
+      <View style={styles.containerB}> </View>
+      <View style={styles.containerC}> </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  containerA: {
+    height: '20%',
+    width: '100%',
+    backgroundColor: '#ff0000',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 25,
+    marginBottom: 10,
   },
+  containerB: {
+    borderRadius: 25,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '50%', 
+    width: '100%',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  containerC: {
+    borderRadius: 25,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '20%',
+    width: '100%',
+    marginTop: 10,
+  },
+  MainContainer:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 30,
+  }
 });
